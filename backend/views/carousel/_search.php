610c02e3ac0c5ef011167c5e86c240f0a298a6fe
chandragie <chandragie@gmail.com>
2015-11-12 00:50:45 +0700

    Add Carousel

---
+++ backend/views/carousel/_search.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\CarouselSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="carousel-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'is_target_self') ?>
+
+    <?= $form->field($model, 'carousel_order') ?>
+
+    <?= $form->field($model, 'image_path') ?>
+
+    <?= $form->field($model, 'image_link') ?>
+
+    <?php // echo $form->field($model, 'caption') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <?php // echo $form->field($model, 'created_by') ?>
+
+    <?php // echo $form->field($model, 'created_date') ?>
+
+    <?php // echo $form->field($model, 'modified_by') ?>
+
+    <?php // echo $form->field($model, 'modified_date') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
