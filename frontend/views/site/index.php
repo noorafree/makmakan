d1f2dc785d54dc4a6ff55d92e21f82129c28bbd1
charizordz <sandy_el.mariachi@yahoo.com>
2015-11-19 23:13:30 +0700

    layar home, sql

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -45,6 +45,8 @@
                         <h3>INDONESIAN FOODS</h3>
                     </a>
                 </div>
+                <div class="col-sm-3">
+                </div>
                 <div class="col-md-4 col-sm-6 col-xs-12 work-space">
                     <a href="images/story3.png" data-lightbox="image-1">
                         <div class="featured-img">
@@ -56,6 +58,8 @@
                         <h3>VEGETARIAN</h3>
                     </a>
                 </div>
+                <div class="col-sm-3">
+                </div>
             </div>
         </div>
     </div>
@@ -65,71 +69,37 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h2 class="text-center features-text">Features</h2>
-                <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-                    <div class="col-md-2 col-sm-2 col-xs-12">
+                <div class="col-md-4 col-sm-12 col-xs-12 icon-box">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
                         <div class="iconing">
                             <i class="glyphicon glyphicon-pencil"></i>
                         </div>
                     </div>
-                    <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                        <h4>Modern Design</h4>
+                    <div class="col-md-12 col-sm-12 col-xs-12 icon-text-box">
+                        <h4>Cash On Delivery</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-                    <div class="col-md-2 col-sm-2 col-xs-12">
+                <div class="col-md-4 col-sm-12 col-xs-12 icon-box">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
                         <div class="iconing">
                             <i class="glyphicon glyphicon-cog"></i>
                         </div>
                     </div>
-                    <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                        <h4>Easy To Customize</h4>
+                    <div class="col-md-12 col-sm-12 col-xs-12 icon-text-box">
+                        <h4>Free Delivery Cost</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-                    <div class="col-md-2 col-sm-2 col-xs-12">
+                <div class="col-md-4 col-sm-12 col-xs-12 icon-box">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
                         <div class="iconing">
                             <i class="glyphicon glyphicon-time"></i>
                         </div>
                     </div>
-                    <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                        <h4>Save Time</h4>
+                    <div class="col-md-12 col-sm-12 col-xs-12 icon-text-box">
+                        <h4>Customer Service</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, adipiscing elit.</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <div class="iconing">
-                            <i class="glyphicon glyphicon-briefcase"></i>
-                        </div>
-                    </div>
-                    <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                        <h4>Professional Work</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <div class="iconing">
-                            <i class="glyphicon glyphicon-user"></i>
-                        </div>
-                    </div>
-                    <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                        <h4>User-friendly Interface</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <div class="iconing">
-                            <i class="glyphicon glyphicon-heart"></i>
-                        </div>
-                    </div>
-                    <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                        <h4>Made With Love</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
                     </div>
                 </div>
             </div>
@@ -150,8 +120,10 @@
                         <div class="image-hover">
                             <i class="glyphicon glyphicon-eye-open"></i>
                         </div>
-                        <h3>Amazing Beauty</h3>
+                        <h3>American Food</h3>
                     </a>
+                    <p class="productname">GRILLED STEAK</p>
+                    <p>IDR 20.000</p>
                 </div>
                 <div class="col-md-4 col-sm-6 col-xs-12 work-space">
                     <a href="images/story2.png" data-lightbox="image-1">
@@ -161,8 +133,10 @@
                         <div class="image-hover">
                             <i class="glyphicon glyphicon-eye-open"></i>
                         </div>
-                        <h3>Mind Blowing</h3>
+                        <h3>American Food</h3>
                     </a>
+                    <p class="productname">GRILLED STEAK</p>
+                    <p>IDR 20.000</p>
                 </div>
                 <div class="col-md-4 col-sm-6 col-xs-12 work-space">
                     <a href="images/story3.png" data-lightbox="image-1">
@@ -172,30 +146,10 @@
                         <div class="image-hover">
                             <i class="glyphicon glyphicon-eye-open"></i>
                         </div>
-                        <h3>Perfect Shot</h3>
+                        <h3>American Food</h3>
                     </a>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 work-space">
-                    <a href="images/story2.png" data-lightbox="image-1">
-                        <div class="featured-img">
-                            <img src="images/story2.png"/>
-                        </div>
-                        <div class="image-hover">
-                            <i class="glyphicon glyphicon-eye-open"></i>
-                        </div>
-                        <h3>Creative thoughts</h3>
-                    </a>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12 work-space">
-                    <a href="images/story3.png" data-lightbox="image-1">
-                        <div class="featured-img">
-                            <img src="images/story3.png"/>
-                        </div>
-                        <div class="image-hover">
-                            <i class="glyphicon glyphicon-eye-open"></i>
-                        </div>
-                        <h3>Beautiful Picture</h3>
-                    </a>
+                    <p class="productname">GRILLED STEAK</p>
+                    <p>IDR 20.000</p>
                 </div>
                 <div class="col-md-4 col-sm-6 col-xs-12 work-space">
                     <a href="images/story1.png" data-lightbox="image-1">
@@ -205,48 +159,37 @@
                         <div class="image-hover">
                             <i class="glyphicon glyphicon-eye-open"></i>
                         </div>
-                        <h3>heart Touching</h3>
+                        <h3>Chinnese Food</h3>
                     </a>
+                    <p class="productname">CAPCAY</p>
+                    <p>IDR 20.000</p>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="container-fluid footer" id="section4">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="col-md-3 col-sm-6 col-xs-12 division">
-                    <h3>CATEGORIES</h3>
-                    <p>American Foods</p>
-                    <p>Indonesian Foods</p>
-                    <p>Asian Foods</p>
-                    <p>Indian Foods</p>
+                <div class="col-md-4 col-sm-6 col-xs-12 work-space">
+                    <a href="images/story2.png" data-lightbox="image-1">
+                        <div class="featured-img">
+                            <img src="images/story2.png"/>
+                        </div>
+                        <div class="image-hover">
+                            <i class="glyphicon glyphicon-eye-open"></i>
+                        </div>
+                        <h3>Chinnese Food</h3>
+                    </a>
+                    <p class="productname">CAPCAY</p>
+                    <p>IDR 20.000</p>
                 </div>
-                <div class="col-md-3 col-sm-6 col-xs-12 division">
-                    <h3>CONTACT US</h3>
-                    <p>Ruko Garden Shopping Arcade Jl. Letjen S.Parman Kav. 28</p
-                    <p>Grogol - Petamburan Jakarta Barat 11470</p>
+                <div class="col-md-4 col-sm-6 col-xs-12 work-space">
+                    <a href="images/story3.png" data-lightbox="image-1">
+                        <div class="featured-img">
+                            <img src="images/story3.png"/>
+                        </div>
+                        <div class="image-hover">
+                            <i class="glyphicon glyphicon-eye-open"></i>
+                        </div>
+                        <h3>Chinnese Food</h3>
+                    </a>
+                    <p class="productname">CAPCAY</p>
+                    <p>IDR 20.000</p>
                 </div>
-                <div class="col-md-3 col-sm-6 col-xs-12 division">
-                    <br/><br/>
-                    <p>Email: sales@makmakan.com
-                    <p>Senin - Jumat: 10.00 - 19.00</p>
-                    <p>Sabtu: 10.00 - 17.00</p>
-                    <p>Minggu: 12.00 - 17.00</p>
-                </div>
-                <div class="col-md-3 col-sm-6 col-xs-12 division">
-                    <h3>KEEP IN TOUCH</h3>
-                    <input style="border-radius: 0;" id="subscriber" type="text" class="form-control" placeholder="enter your email here"/>
-                    <h3>BE SOCIAL</h3>
-                    <a href="#"><img src="images/facebook.gif" style="width: 20%; height: auto;"/></a>
-                    <a href="#"><img src="images/twitter.gif" style="width: 20%; height: auto;"/></a>
-                    <a href="#"><img src="images/gplus.gif" style="width: 20%; height: auto;"/></a>
-                </div>
-            </div>
-            <div class="col-md-12 copyright">
-                <p>Copyright &copy; 2015 Makmakan is a registered trademark</p>
             </div>
         </div>
     </div>
