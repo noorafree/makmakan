a59e0588c72c6b9b5700f093b2dc3d37768ef0f5
tsuwandi <timotius.suwandi@gmail.com>
2015-12-25 15:57:21 +0700

    [TSI][20151225] Update detail product belum fix

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -15,21 +15,27 @@ use yii\web\AssetBundle;
  * @since 2.0
  */
 class AppAsset extends AssetBundle {
-    
+
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/bootstrap.css',
         'css/bootstrap.min.css',
         'css/bootstrap.offcanvas.min.css',
         'css/bootstrap-timepicker.min.css',
         'custom-style.css',
         'css/lightbox.css',
         'style.css',
+        'css/style2.css',
+        'css/style3.css',
+        'css/jquery-ui.css',
+        'css/nouislider.css',
+        'css/nouislider.pips.css',
+        'css/nouislider.tooltips.css',
         'https://fonts.googleapis.com/css?family=Lato:400,400italic,700,700italic'
 //        'https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js',
 //        'https://oss.maxcdn.com/respond/1.4.2/respond.min.js',
     ];
-    
     public $js = [
 //        'js/jquery-2.1.4.min.js',
         'js/bootstrap.min.js',
@@ -40,8 +46,12 @@ class AppAsset extends AssetBundle {
         'js/modal.js',
         'js/ddaccordion.js',
         'js/usermenu.js',
+//        'js/main.js',
+//        'js/jquery.min.js',
+//        'js/jquery-ui.js',
+//        'js/jquery-ui.min.js',
+        'js/nouislider.js',
     ];
-    
     public $depends = [
         'yii\web\YiiAsset',
             //'yii\bootstrap\BootstrapAsset',
